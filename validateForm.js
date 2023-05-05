cc3c0952d8434799525eb581f3448169656aad12
Maxime Lecieux <maxime.lecieuxlmf@gmail.com>
2023-05-05 11:48:12 +0200

    Fixed functioning of login modal validation script + fixed login modal code on html pages

--- validateForm.js
+++ validateForm.js
@@ -3,6 +3,30 @@ let connexionForm = document.getElementById('connexionForm')
 let allergyYesCheck = document.getElementById('allergyYes')
 let validatePasswordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/ // Regex for strong password
 
+connexionForm.addEventListener('submit', function(e){ // validate form for modal connexion
+    let emailConnexionForm = document.getElementById('emailConnexionForm')
+    let passwordConnexionForm = document.getElementById('passwordConnexionForm')
+
+    if (emailConnexionForm.value.trim() === ""){
+        let errorEmailConnexionForm = document.getElementById('errorEmailConnexionForm')
+        errorEmailConnexionForm.innerHTML = "Une adresse mail est requis"
+        errorEmailConnexionForm.style.color = "red"
+        e.preventDefault()
+    } 
+
+    if (passwordConnexionForm.value.trim() === ""){
+        let errorPasswordConnexionForm = document.getElementById('errorPasswordConnexionForm')
+        errorPasswordConnexionForm.innerHTML = "Un mot de passe est requis"
+        errorPasswordConnexionForm.style.color = "red"
+        e.preventDefault()
+    } else if (validatePasswordRegex.test(passwordConnexionForm.value) === false){
+        let errorPasswordConnexionForm = document.getElementById('errorPasswordConnexionForm')
+        errorPasswordConnexionForm.innerHTML = "Mot de passe invalide"
+        errorPasswordConnexionForm.style.color = "red"
+        e.preventDefault()
+    }
+})
+
 allergyYesCheck.addEventListener('change', function(e){ // input creation function to indicate allergies
     let parent = document.getElementById('allergyYesChecked')
     let allergies = document.createElement("INPUT");
@@ -43,28 +67,4 @@ registrationForm.addEventListener('submit', function(e){ // validate form for re
         errorNumberCoversRegistrationForm.style.color = "red"
         e.preventDefault()
         }
-})
-
-connexionForm.addEventListener('submit', function(e){ // validate form for modal connexion
-    let emailConnexionForm = document.getElementById('emailConnexionForm')
-    let passwordConnexionForm = document.getElementById('passwordConnexionForm')
-
-    if (emailConnexionForm.value.trim() === ""){
-        let errorEmailConnexionForm = document.getElementById('errorEmailConnexionForm')
-        errorEmailConnexionForm.innerHTML = "Une adresse mail est requis"
-        errorEmailConnexionForm.style.color = "red"
-        e.preventDefault()
-    } 
-
-    if (passwordConnexionForm.value.trim() === ""){
-        let errorPasswordConnexionForm = document.getElementById('errorPasswordConnexionForm')
-        errorPasswordConnexionForm.innerHTML = "Un mot de passe est requis"
-        errorPasswordConnexionForm.style.color = "red"
-        e.preventDefault()
-    } else if (validatePasswordRegex.test(passwordConnexionForm.value) === false){
-        let errorPasswordConnexionForm = document.getElementById('errorPasswordConnexionForm')
-        errorPasswordConnexionForm.innerHTML = "Mot de passe invalide"
-        errorPasswordConnexionForm.style.color = "red"
-        e.preventDefault()
-    }
 })
